ff543034c049b4e4a5b2536e55d52ee696a0e8eb
Meagan Brown <meaganbrown15@gmail.com>
2022-08-23 22:25:27 +0000

    .

--- R/aloha.R
+++ R/aloha.R
@@ -8,12 +8,8 @@
 #' @export
 #'
 #' @examples say_aloha("Allison")
-<<<<<<< HEAD
 
 say_aloha <- function(name) {
-=======
-say_aloha <- function(name, back_color, font_color = NA) {
->>>>>>> 4729bbb1bb61130db68cd6defcf40350669cd6b6
 
   message <- paste("Aloha,",
                    name,
@@ -21,10 +17,7 @@ say_aloha <- function(name, back_color, font_color = NA) {
                    emo::ji("sunny"),
                    emo::ji("ocean"))
 
-<<<<<<< HEAD
   cat(crayon::yellow$bgBlue(message))
-=======
-  cat(crayon::bgBlue(message))
->>>>>>> 4729bbb1bb61130db68cd6defcf40350669cd6b6
+
 }
 
